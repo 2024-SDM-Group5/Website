9b0cbb279e5d2e91884e50fb85daa27aa73cbc9b
Joshuaoneheart <b08902017@csie.ntu.edu.tw>
2024-03-23 22:59:15 +0800

    add map

--- src/app/(main-interface)/map/general/page.tsx
+++ src/app/(main-interface)/map/general/page.tsx
@@ -1,10 +1,64 @@
+'use client';
+
 import React from 'react';
+import { useForm } from 'react-hook-form';
+
+import GoogleMapReact from 'google-map-react';
+
+import { Form, FormItem, FormField, FormControl } from '@/components/ui/form';
+import { Input } from "@/components/ui/input"
+
+interface Coordinate {
+	lat: number | undefined;
+	lng: number | undefined;
+}
 
 function HomePage() {
+	const [bound, setBound] = React.useState<Array<Coordinate>>([
+		{
+			lat: 10.99835602,
+			lng: 77.01502627,
+		},
+		{
+			lat: 10.99835602,
+			lng: 77.01502627,
+		},
+	]);
+	const form = useForm();
+	console.log(bound);
 	return (
-		<div>
-			<h1>Welcome to the Home Page</h1>
-			<p>This is a sample home page.</p>
+		<div style={{ width: '100vw', height: 'calc(100vh - 140px)' }}>
+			<Form {...form}>
+				<form>
+					<FormField
+						control={form.control}
+						name="username"
+						render={({ field }) => (
+							<FormItem>
+								<FormControl>
+									<Input placeholder="shadcn" {...field} />
+								</FormControl>
+							</FormItem>
+						)}
+					/>
+				</form>
+			</Form>
+			<GoogleMapReact
+				bootstrapURLKeys={{ key: 'AIzaSyCZR4VCUOau8mVfA7CmTg9rMM6BpJF8f9o' }}
+				defaultCenter={{
+					lat: 10.99835602,
+					lng: 77.01502627,
+				}}
+				defaultZoom={9}
+				onDragEnd={(map: google.maps.Map) => {
+					let ne = map.getBounds()?.getNorthEast();
+					let sw = map.getBounds()?.getSouthWest();
+					setBound([
+						{ lat: ne?.lat(), lng: ne?.lng() },
+						{ lat: sw?.lat(), lng: sw?.lng() },
+					]);
+				}}
+			></GoogleMapReact>
 		</div>
 	);
 }
