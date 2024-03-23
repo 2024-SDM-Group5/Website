fd4e9efbac31c585599010bbb92b2ab53795cfc3
Joshuaoneheart <b08902017@csie.ntu.edu.tw>
2024-03-24 00:27:44 +0800

    add map/map/overview

--- src/app/(main-interface)/map/general/page.tsx
+++ src/app/(main-interface)/map/general/page.tsx
@@ -1,12 +1,11 @@
 'use client';
 
 import React from 'react';
-import { useForm } from 'react-hook-form';
 
+import { SearchOutlined } from '@ant-design/icons';
+import { Input } from 'antd';
 import GoogleMapReact from 'google-map-react';
 
-import { Form, FormItem, FormField, FormControl } from '@/components/ui/form';
-import { Input } from "@/components/ui/input"
 
 interface Coordinate {
 	lat: number | undefined;
@@ -24,25 +23,28 @@ function HomePage() {
 			lng: 77.01502627,
 		},
 	]);
-	const form = useForm();
-	console.log(bound);
 	return (
-		<div style={{ width: '100vw', height: 'calc(100vh - 140px)' }}>
-			<Form {...form}>
-				<form>
-					<FormField
-						control={form.control}
-						name="username"
-						render={({ field }) => (
-							<FormItem>
-								<FormControl>
-									<Input placeholder="shadcn" {...field} />
-								</FormControl>
-							</FormItem>
-						)}
-					/>
-				</form>
-			</Form>
+		<div
+			style={{
+				width: '100vw',
+				height: 'calc(100vh - 148px)',
+				marginTop: '4px',
+				marginBottom: '4px',
+			}}
+		>
+			<div
+				className="w-full"
+				style={{
+					padding: '5px',
+					paddingTop: '9px',
+					position: 'absolute',
+					top: '104px',
+					zIndex: 10,
+				}}
+			>
+				<Input prefix={<SearchOutlined />} />
+			</div>
+
 			<GoogleMapReact
 				bootstrapURLKeys={{ key: 'AIzaSyCZR4VCUOau8mVfA7CmTg9rMM6BpJF8f9o' }}
 				defaultCenter={{
