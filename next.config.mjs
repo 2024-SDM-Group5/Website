770b6ac1e9e3c02e9d3d32c2d62eddb86478f9d5
Joshuaoneheart <b08902017@csie.ntu.edu.tw>
2024-04-02 17:17:00 +0800

    fix config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {assetPrefix: "https://pc214.ee.ntu.edu.tw/website",
-					basePath: "https://pc214.ee.ntu.edu.tw/website"};
+					basePath: "/website"};
 
 export default nextConfig;
