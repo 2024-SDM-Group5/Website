bdc4fcf5ab148116f8f603e68120a7ae77e4b961
Claire0512 <claire32246223@gmail.com>
2024-04-13 01:24:08 +0800

    Fix navbar and footer redirecting problem

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,6 @@
 
 const isProd = process.env.NODE_ENV === 'production'
 
-const nextConfig = { reactStrictMode: false, assetPrefix: isProd ? 'https://pc214.ee.ntu.edu.tw/website' : undefined };
+const nextConfig = { reactStrictMode: false, assetPrefix: isProd ? 'https://pc214.ee.ntu.edu.tw/website' : undefined, basePath: '/website' };
 
 export default nextConfig;
