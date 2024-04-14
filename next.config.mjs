8ee5bf23ce7dfe4b428f440f6b5e0d894cc145ee
Claire0512 <claire32246223@gmail.com>
2024-04-14 21:58:01 +0800

    Fix deploy error

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production';
+const isDev = process.env.NODE_ENV === 'development';
 
 const nextConfig = {
 	reactStrictMode: false,
-	assetPrefix: isProd ? 'https://pc214.ee.ntu.edu.tw/website' : undefined,
-	basePath: '/website',
+	assetPrefix: isDev ? undefined : 'https://pc214.ee.ntu.edu.tw/website',
+	basePath: isDev ? '/website' : undefined,
 };
 
 export default nextConfig;
