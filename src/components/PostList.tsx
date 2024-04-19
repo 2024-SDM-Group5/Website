a324fa2c03f5c39402d16597bc432121edf1dd30
Claire0512 <claire32246223@gmail.com>
2024-04-19 17:10:31 +0800

    Adjust lint setting

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -3,7 +3,7 @@ import type { SinglePostType } from './SinglePost';
 
 const PostList = ({ posts }: { posts: SinglePostType[] }) => (
 	<div className="h-[100vh] overflow-hidden overflow-y-scroll">
-		{posts.map((post) => (
+		{/* {posts.map((post) => (
 			<SinglePost
 				key={post.id}
 				authorAvatarUrl={post.authorAvatarUrl}
@@ -12,7 +12,7 @@ const PostList = ({ posts }: { posts: SinglePostType[] }) => (
 				favCount={post.favCount}
 				replies={post.replies}
 			/>
-		))}
+		))} */}
 	</div>
 );
 export default PostList;
