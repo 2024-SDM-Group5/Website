5ea6549a3459cd95743447d9eb2c7d13883b843a
Claire0512 <claire32246223@gmail.com>
2024-04-22 14:47:30 +0800

    Implement diary list fetching for the overview page and following page of the community

--- src/app/(main-interface)/community/following/page.tsx
+++ src/app/(main-interface)/community/following/page.tsx
@@ -1,18 +1,45 @@
 'use client';
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { useSession } from 'next-auth/react';
+
+import axios from 'axios';
 
 import PostList from '@/components/PostList';
 
-const posts = [
-	{
-		diaryId: 1,
-	},
-	{
-		diaryId: 2,
-	},
-];
+interface Diary {
+	id: number;
+	imageUrl: string;
+	restaurantName: string;
+}
 
 export default function Page() {
+	const { data: session } = useSession();
+	const [posts, setPosts] = useState<Array<{ diaryId: number }>>([]);
+	useEffect(() => {
+		const fetchDiaries = async () => {
+			if (session?.idToken) {
+				try {
+					const response = await axios.get<Diary[]>(
+						'https://mainserver-fdhzgisj6a-de.a.run.app/api/v1/diaries',
+						{
+							params: { following: true },
+							headers: { Authorization: `Bearer ${session.idToken}` },
+						},
+					);
+					const diaryPosts = response.data.map((diary: Diary) => ({
+						diaryId: diary.id,
+					}));
+					setPosts(diaryPosts);
+				} catch (error) {
+					console.error('Failed to fetch diaries:', error);
+				}
+			}
+		};
+
+		fetchDiaries();
+	}, [session]);
+
 	return <PostList posts={posts} />;
 }
